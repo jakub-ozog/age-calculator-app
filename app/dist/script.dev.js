bd2847e25c6cee01b9b0003db23a4bdb26239e6a
Jakub Ozog <jakubjanozog@gmail.com>
2023-08-20 20:01:22 +0200

    inputs/js logic for date counting update

--- app/dist/script.dev.js
+++ app/dist/script.dev.js
@@ -1,14 +1,51 @@
 "use strict";
 
-var calculateTime = function calculateTime() {
-  var dayInput = new Date(document.querySelector("#dayInput").value);
-  var monthInput = new Date(document.querySelector("#monthInput").value);
-  var yearInput = new Date(document.querySelector("#yearInput").value);
+// const calculateTime = () => {
+//   const day = parseInt(document.querySelector("#dayInput").value);
+//   const month = parseInt(document.querySelector("#monthInput").value);
+//   const year = parseInt(document.querySelector("#yearInput").value);
+//   const inputDate = new Date(year, month, day);
+//   const currentDate = new Date();
+//   const timeDifference = Math.abs(currentDate - inputDate);
+//   const dayDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+//   const monthDifference = Math.floor(dayDifference / 30.44);
+//   const yearDifference = Math.floor(dayDifference / 365.25);
+//   document.querySelector(".result__yearDiff").innerHTML = `
+//   ${yearDifference}
+//   `;
+//   document.querySelector(".result__monthDiff").innerHTML = `
+//   ${monthDifference}
+//   `;
+//   document.querySelector(".result__dayDiff").innerHTML = `
+//   ${dayDifference}
+//   `;
+// };
+// document.querySelector(".submitBtn").addEventListener("click", calculateTime);
+// const age = () => {
+//   const d1 = document.querySelector('#day').value
+//   const m1 = document.querySelector('#month').value
+//   const y1 = document.querySelector('#year').value
+//   const date = new Date();
+//   const d2 = date.getDate();
+//   const m2 = date.getMonth() + 1;
+//   const y2 = date.getFullYear();
+// }
+document.querySelector(".submitBtn").addEventListener("click", function () {
+  var birthDay = parseInt(document.querySelector("#dayInput").value);
+  var birthMonth = parseInt(document.querySelector("#monthInput").value) - 1;
+  var birthYear = parseInt(document.querySelector("#yearInput").value);
+  var birthDate = new Date(birthYear, birthMonth, birthDay);
   var currentDate = new Date();
-  var daysDifference = Math.floor((currentDate - dayInput) / (1000 * 60 * 60 * 24));
-  var monthDifference = Math.floor(daysDifference / 30.44);
-  var yearDifference = Math.floor(daysDifference / 365.25);
-  document.querySelector(".result__timeDiff").innerHTML = "\n  Days: ".concat(daysDifference, " <br>\n  Months: ").concat(monthDifference, " <br>\n  Years: ").concat(yearDifference, " <br>\n  ");
-};
+  var ageInMilliseconds = currentDate - birthDate;
+  var ageInSeconds = ageInMilliseconds / 1000;
+  var ageInMinutes = ageInSeconds / 60;
+  var ageInHours = ageInMinutes / 60;
+  var ageInDays = ageInHours / 24;
+  var years = Math.floor(ageInDays / 365);
+  var remainingDays = ageInDays % 365;
+  var months = Math.floor(remainingDays / 30); // Przybliżona liczba miesięcy
 
-document.querySelector(".submitBtn").addEventListener("click", calculateTime);
+  var days = Math.floor(remainingDays % 30); // Przybliżona liczba dni
+
+  document.querySelector(".result__dateGeneral").innerHTML = "\n    Age: ".concat(years, " years, ").concat(months, " months, ").concat(days, " days\n  ");
+});
--- app/script.js
+++ app/script.js
@@ -1,18 +1,66 @@
-const calculateTime = () => {
-  const dayInput = new Date(document.querySelector("#dayInput").value);
-  const monthInput = new Date(document.querySelector("#monthInput").value);
-  const yearInput = new Date(document.querySelector("#yearInput").value);
-  const currentDate = new Date();
+// const calculateTime = () => {
+//   const day = parseInt(document.querySelector("#dayInput").value);
+//   const month = parseInt(document.querySelector("#monthInput").value);
+//   const year = parseInt(document.querySelector("#yearInput").value);
 
-  const daysDifference = Math.floor((currentDate - dayInput) / (1000 * 60 * 60 * 24));
-  const monthDifference = Math.floor((daysDifference) / 30.44);
-  const yearDifference = Math.floor((daysDifference) / 365.25);
+//   const inputDate = new Date(year, month, day);
+//   const currentDate = new Date();
 
-  document.querySelector(".result__timeDiff").innerHTML = `
-  Days: ${daysDifference} <br>
-  Months: ${monthDifference} <br>
-  Years: ${yearDifference} <br>
+//   const timeDifference = Math.abs(currentDate - inputDate);
+//   const dayDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+//   const monthDifference = Math.floor(dayDifference / 30.44);
+//   const yearDifference = Math.floor(dayDifference / 365.25);
+
+//   document.querySelector(".result__yearDiff").innerHTML = `
+//   ${yearDifference}
+//   `;
+
+//   document.querySelector(".result__monthDiff").innerHTML = `
+//   ${monthDifference}
+//   `;
+
+//   document.querySelector(".result__dayDiff").innerHTML = `
+//   ${dayDifference}
+//   `;
+// };
+
+// document.querySelector(".submitBtn").addEventListener("click", calculateTime);
+
+// const age = () => {
+
+//   const d1 = document.querySelector('#day').value
+//   const m1 = document.querySelector('#month').value
+//   const y1 = document.querySelector('#year').value
+
+//   const date = new Date();
+//   const d2 = date.getDate();
+//   const m2 = date.getMonth() + 1;
+//   const y2 = date.getFullYear();
+
+// }
+
+
+document.querySelector(".submitBtn").addEventListener("click", () => {
+const birthDay = parseInt(document.querySelector("#dayInput").value);
+const birthMonth = parseInt(document.querySelector("#monthInput").value) - 1;
+const birthYear = parseInt(document.querySelector("#yearInput").value);
+
+const birthDate = new Date(birthYear, birthMonth, birthDay);
+const currentDate = new Date();
+
+const ageInMilliseconds = currentDate - birthDate;
+const ageInSeconds = ageInMilliseconds / 1000;
+const ageInMinutes = ageInSeconds / 60;
+const ageInHours = ageInMinutes / 60;
+const ageInDays = ageInHours / 24;
+
+const years = Math.floor(ageInDays / 365);
+const remainingDays = ageInDays % 365;
+const months = Math.floor(remainingDays / 30); // Przybliżona liczba miesięcy
+const days = Math.floor(remainingDays % 30); // Przybliżona liczba dni
+
+document.querySelector(".result__dateGeneral").innerHTML = `
+    Age: ${years} years, ${months} months, ${days} days
   `;
-};
+});
 
-document.querySelector(".submitBtn").addEventListener("click", calculateTime);
